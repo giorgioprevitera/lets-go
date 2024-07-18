1da2724a8738fa71ba3dd760442da9b3fabd478b
Giorgio Previtera <giorgioprevitera@gmail.com>
2024-07-18 14:24:21 +0100

    Add user authentication

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,14 +16,16 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("GET /{$}", dynamicMiddleware.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamicMiddleware.ThenFunc(app.snippetView))
-	mux.Handle("GET /snippet/create", dynamicMiddleware.ThenFunc(app.snippetCreate))
-	mux.Handle("POST /snippet/create", dynamicMiddleware.ThenFunc(app.snippetCreatePost))
-
 	mux.Handle("GET /user/signup", dynamicMiddleware.ThenFunc(app.userSignup))
 	mux.Handle("POST /user/signup", dynamicMiddleware.ThenFunc(app.userSignupPost))
 	mux.Handle("GET /user/login", dynamicMiddleware.ThenFunc(app.userLogin))
 	mux.Handle("POST /user/login", dynamicMiddleware.ThenFunc(app.userLoginPost))
-	mux.Handle("POST /user/logout", dynamicMiddleware.ThenFunc(app.userLogoutPost))
+
+	protectedMiddleware := dynamicMiddleware.Append(app.requireAuthentication)
+
+	mux.Handle("GET /snippet/create", protectedMiddleware.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", protectedMiddleware.ThenFunc(app.snippetCreatePost))
+	mux.Handle("POST /user/logout", protectedMiddleware.ThenFunc(app.userLogoutPost))
 
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 	return standardMiddleware.Then(mux)
